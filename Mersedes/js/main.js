fa7c6ed5de33499a1c48532bca24b1333df90d78
RadostDudhi <rudakova1720@gmail.com>
2022-05-29 19:14:37 +0300

    add design-block

--- Mersedes/js/main.js
+++ Mersedes/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $(".design__slider").slick({
+        dots:false,
+        slidesToShow: 4,
+        variableWidth: true,
+        prevArrow: '<img class="arrow arrow-left" src="images/left.png" alt="" />',
+        nextArrow:'<img class="arrow arrow-right" src="images/right.png" alt="" />',
+    });
 });
