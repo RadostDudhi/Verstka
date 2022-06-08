4b2a0da59a4483d17454184b0dc715cae620a619
RadostDudhi <rudakova1720@gmail.com>
2022-06-08 21:44:42 +0300

    add block slider

--- cosmic/js/main.js
+++ cosmic/js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/prev.svg" alt="arrow-left" />',
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/next.svg" alt="arrow-right" />',
+    });
+    
 });
